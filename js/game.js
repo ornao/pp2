0833f28b181aa3a2d30c28710efc2e0279979971
ornao <reynoldsorna@gmail.com>
2023-01-30 11:38:44 +0000

    add hud (video 5)

--- js/game.js
+++ js/game.js
@@ -1,5 +1,7 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
+const questionCounterText = document.getElementById("questionCounter");
+const scoreText = document.getElementById("score");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -52,6 +54,8 @@ getNewQuestion = () => {
         return window.location.assign('/end.html');
     }
     questionCounter++;
+    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+
     const questionIndex = Math.floor(Math.random() * availableQuesions.length);
     currentQuestion = availableQuesions[questionIndex];
     question.innerText = currentQuestion.question;
@@ -75,6 +79,10 @@ choices.forEach(choice => {
   
       const classToApply =
         selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+        if (classToApply === "correct") {
+            incrementScore(CORRECT_BONUS);
+        }
   
       selectedChoice.parentElement.classList.add(classToApply);
   
@@ -85,4 +93,9 @@ choices.forEach(choice => {
     });
   });
   
+  incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+  };
+
   startGame();
